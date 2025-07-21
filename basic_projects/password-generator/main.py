ca35d871294985e5aff8e9be1c944ee88f85a1e3
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-07-21 20:22:26 +0530

    added new project

---
+++ basic_projects/password-generator/main.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+length = int(input("Enter the length of password: "))
+
+print('''choose the character set for password :
+                  1. numbers
+                  2. letters
+                  3. special characters
+                  4. exit''')
+
+ch_list = ""
+
+while (True):
+    choice = int(input("Pick a number "))
+    if (choice == 1):
+        ch_list +=string.digits
+    elif (choice == 2):
+        ch_list +=string.ascii_letters
+    elif (choice == 3):
+        ch_list += string.punctuation
+    elif (choice == 4):
+        break
+    else:
+        print("pick a valid number")
+
+password = []
+for i in range(length):
+    randomchar = random.choice(ch_list)
+    password.append(randomchar)
+
+print("The random password is " + "".join(password))
