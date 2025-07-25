18e9e4bbdc4128ca164c3c3824c7772db1c46091
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-07-25 22:05:33 +0530

    added new project

---
+++ basic_projects/pdf-to-image/main.py
@@ -0,0 +1,6 @@
+from pdf2image import convert_from_path
+
+old_pdf = convert_from_path(r"C:\Users\HP\Downloads\AdmitCard-253510000051.pdf", poppler_path=r"C:\Users\HP\OneDrive\Desktop\Python-projects\basic_projects\pdf-to-image\poppler-24.08.0\Library\bin")
+
+for i in range(len(old_pdf)):
+    old_pdf[i].save("new"+str(i)+".jpg","JPEG")
