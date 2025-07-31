fc63edfff0c5132159677540a606c67bd842597c
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-07-31 18:32:24 +0530

    updated code

--- basic_projects/pdf-to-image/main.py
+++ basic_projects/pdf-to-image/main.py
@@ -1,6 +1,6 @@
 from pdf2image import convert_from_path
 
-old_pdf = convert_from_path(r"C:\Users\HP\Downloads\AdmitCard-253510000051.pdf", poppler_path=r"C:\Users\HP\OneDrive\Desktop\Python-projects\basic_projects\pdf-to-image\poppler-24.08.0\Library\bin")
+old_pdf = convert_from_path(r"C:\Users\HP\Downloads\93a91655-0f46-4ef5-bc64-fa5741185488.pdf", poppler_path=r"C:\Users\HP\OneDrive\Desktop\Python-projects\basic_projects\pdf-to-image\poppler-24.08.0\Library\bin")
 
 for i in range(len(old_pdf)):
     old_pdf[i].save("new"+str(i)+".jpg","JPEG")
--- basic_projects/rent-calculator/main.py
+++ basic_projects/rent-calculator/main.py
@@ -8,3 +8,11 @@ total_bill = elec*charge_per_unit
 
 total = (food + rent + total_bill) // pers
 print("each person rent is : ",total)
+
+# output :
+# enter your flat rent :5000
+# enter your food expenses :2500
+# enter your electricity bill :1500
+# enter your charge per unit :10
+# enter the no. of persons living in flat :2
+# each person rent is :  11250
